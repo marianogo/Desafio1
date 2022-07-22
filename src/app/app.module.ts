27d563dce2fdb86317bfaed34e8faa6ce3256a56
marianogo <marianogoyochea@gmail.com>
2022-07-21 22:17:06 -0300

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ContentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
